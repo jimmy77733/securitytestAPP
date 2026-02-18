d9bd759432ddea27f0f8ceb1ecb42cdac3973d4b
jimmy77733 <jimmy880702@gmail.com>
2026-02-19 00:23:56 +0800

    checkQUS_112-2

--- src/components/QuestionImageModal.tsx
+++ src/components/QuestionImageModal.tsx
@@ -33,9 +33,9 @@ export const QuestionImageModal: React.FC<QuestionImageModalProps> = ({
           {/* Modal 內容 */}
           <motion.div
             className="question-image-modal"
-            initial={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.9 }}
+            initial={{ opacity: 0, scale: 0.9, x: '-50%', y: '-50%' }}
+            animate={{ opacity: 1, scale: 1, x: '-50%', y: '-50%' }}
+            exit={{ opacity: 0, scale: 0.9, x: '-50%', y: '-50%' }}
             onClick={(e) => e.stopPropagation()}
           >
             <div className="question-image-modal-header">
